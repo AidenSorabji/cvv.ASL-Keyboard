8e9accf6c057e472a686a585a8d7864280d35a1c
Aiden Sorabji <sorabjiaiden@gmail.com>
2024-09-17 09:17:16 -0600

    modified main.py to include some modules

---
+++ main.py
@@ -0,0 +1,12 @@
+# -------------------- Imports --------------------  #
+# Mediapipe
+import mediapipe as mp
+from mediapipe.tasks.python import vision
+from mediapipe.tasks.python import text
+from mediapipe.tasks.python import audio
+
+# OpenCV
+import OpenCV
+
+# Tenserflow
+import tenserflow as tenser
